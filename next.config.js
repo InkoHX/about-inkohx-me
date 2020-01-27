bfebc5513d28a12820bfdc2c7550f07f9b19e11a
InkoHX <inkohx@gmail.com>
2020-01-27 23:05:04 +0900

    Fix webpack config.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
   webpack: (config) => {
-    console.log(config)
     config.resolve.plugins.push(new TsconfigPathsPlugin())
+
+    return config
   }
 }
