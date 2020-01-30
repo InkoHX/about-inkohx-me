e31957d3c488d800f191d0d4a2df3f5d15c81493
InkoHX <inkohx@gmail.com>
2020-01-30 12:03:06 +0900

    Add _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
+
+export default class extends Document {
+  public static async getInitialProps (ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  public render () {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
