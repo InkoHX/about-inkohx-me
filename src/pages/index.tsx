89e6ebdc7177632bc99d186eb6fede8c44a57c49
InkoHX <inkohx@gmail.com>
2020-01-30 22:24:43 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
+import React from 'react'
 import { NextPage } from 'next'
 import MainLayout from '@Components/MainLayout'
-import { Typography } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import makeStyle from '@material-ui/core/styles/makeStyles'
+import Avatar from '@material-ui/core/Avatar'
+import { createStyles } from '@material-ui/core'
+
+const MainProfile: React.FunctionComponent = () => {
+  const style = makeStyle(theme => createStyles({
+    root: {
+      marginTop: '3%'
+    },
+    avatar: {
+      width: theme.spacing(20),
+      height: theme.spacing(20),
+      margin: '0 auto',
+      marginBottom: '3%'
+    }
+  }))()
+
+  return (
+    <Typography align='center' component='div' className={style.root}>
+      <Avatar className={style.avatar} alt='InkoHX icon' src='/images/index/inkohx-icon.gif' />
+      <Typography variant='h5' component='h1'>毎日コード書いてGitHubにコミットしまくってる学生です。</Typography>
+    </Typography>
+  )
+}
 
 const index: NextPage = () => {
   return (
     <MainLayout>
-      <Typography variant='h1'>About InkoHX</Typography>
+      <MainProfile />
     </MainLayout>
   )
 }
