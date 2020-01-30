5e948c3bbddfab099154b7263330304d1fdb5c38
InkoHX <inkohx@gmail.com>
2020-01-31 00:06:21 +0900

    見やすくしてみた。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,38 @@ import Avatar from '@material-ui/core/Avatar'
 import { createStyles } from '@material-ui/core'
 
 const MainProfile: React.FunctionComponent = () => {
-  const style = makeStyle(theme => createStyles({
-    root: {
-      marginTop: '3%'
-    },
-    avatar: {
-      width: theme.spacing(20),
-      height: theme.spacing(20),
-      margin: '0 auto',
-      marginBottom: '3%'
-    }
-  }))()
+  const style = makeStyle(theme =>
+    createStyles({
+      root: {
+        marginTop: '3%'
+      },
+      avatar: {
+        width: theme.spacing(20),
+        height: theme.spacing(20),
+        margin: '0 auto',
+        marginBottom: '3%'
+      }
+    })
+  )()
 
   return (
-    <Typography align='center' component='div' className={style.root}>
-      <Avatar className={style.avatar} alt='InkoHX icon' src='/images/index/inkohx-icon.gif' />
-      <Typography variant='h5' component='h1'>毎日コード書いてGitHubにコミットしまくってる学生です。</Typography>
+    <Typography
+      align='center'
+      component='div'
+      className={style.root}
+    >
+      <Avatar
+        className={style.avatar}
+        alt='InkoHX icon'
+        src='/images/index/inkohx-icon.gif'
+      />
+
+      <Typography
+        variant='h5'
+        component='h1'
+      >
+        毎日コード書いてGitHubにコミットしまくってる学生です。
+      </Typography>
     </Typography>
   )
 }
