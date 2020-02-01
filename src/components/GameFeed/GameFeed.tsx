fc3edd48e92b643ece60d65ec1b38740753dd12c
InkoHX <inkohx@gmail.com>
2020-02-01 21:16:55 +0900

    minor fix

--- src/components/GameFeed/GameFeed.tsx
+++ src/components/GameFeed/GameFeed.tsx
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 
 import { Duration } from '@icholy/duration'
+import Container from '@material-ui/core/Container'
 
 type GameFeedProps = {
   response: SteamResponse
@@ -19,59 +20,61 @@ const GameFeed: React.FunctionComponent<GameFeedProps> = ({
 }) => {
   const style = makeStyles({
     card: {
-      width: 470,
-      height: 300
+      width: 350
     }
   })()
 
   const data = response.response.games
 
   return (
-    <Gird container spacing={3} alignItems='center' justify='center'>
-      {
-        data.map((value) => {
-          if (!value.img_icon_url) return
-          if (!value.img_logo_url) return
-          if (!value.name) return
+    <Container>
+      <Gird container spacing={2} alignItems='center' justify='center'>
+        {
+          data.map((value) => {
+            if (!value.img_icon_url) return
+            if (!value.img_logo_url) return
+            if (!value.name) return
 
-          const url = `https://steamcdn-a.akamaihd.net/steam/apps/${value.appid}/header.jpg`
-          const gameUrl = `https://store.steampowered.com/app/${value.appid}`
+            const url = `https://steamcdn-a.akamaihd.net/steam/apps/${value.appid}/header.jpg`
+            const gameUrl = `https://store.steampowered.com/app/${value.appid}`
 
-          const towWeeks = new Duration(`${value.playtime_2weeks}m`).toString()
-          const forever = new Duration(`${value.playtime_forever}m`).toString()
+            const towWeeks = new Duration(`${value.playtime_2weeks}m`).toString()
+            const forever = new Duration(`${value.playtime_forever}m`).toString()
 
-          return (
-            <Gird item key={value.appid}>
-              <Card className={style.card}>
-                <CardActionArea href={gameUrl}>
-                  <CardMedia
-                    component='img'
-                    image={url}
-                    alt={value.name}
-                    title={value.name}
-                    width='150'
-                    height='150'
-                  />
-                </CardActionArea>
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant='h5'
-                    component='h3'
-                  >
-                    {value.name}
-                  </Typography>
-                  <Typography align='left' variant='subtitle1' component='p'>
-                    過去二週間: {towWeeks}<br />
-                    合計: {forever}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Gird>
-          )
-        })
-      }
-    </Gird>
+            return (
+              <Gird item key={value.appid}>
+                <Card className={style.card}>
+                  <CardActionArea href={gameUrl}>
+                    <CardMedia
+                      component='img'
+                      image={url}
+                      alt={value.name}
+                      title={value.name}
+                      width='150'
+                      height='150'
+                    />
+                  </CardActionArea>
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant='h6'
+                      component='h3'
+                      noWrap
+                    >
+                      {value.name}
+                    </Typography>
+                    <Typography align='left' variant='subtitle1' component='p'>
+                      過去二週間: {towWeeks}<br />
+                      合計: {forever}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Gird>
+            )
+          })
+        }
+      </Gird>
+    </Container>
   )
 }
 
